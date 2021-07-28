902e5602f4ebbf3363206e34e17795f2ec9661f5
wangjiaji123 <1639051460@qq.com>
2021-07-28 23:10:08 +0800

    Initial commit

---
+++ module01/src/com/atguigu/module/Hellow.java
@@ -0,0 +1,11 @@
+package com.atguigu.module;
+
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("Hellow word!");
+    }
+
+    public  void name(){
+
+    }
+}
---
+++ src/com/atguigu/java/HellowWord.java
@@ -0,0 +1,7 @@
+package com.atguigu.java;
+
+public class HellowWord {
+    public static void main(String[] args) {
+        System.out.println("Hellow word!");
+    }
+}
